a25654cd5192801180206cbfc34979e54eacd0c9
Dimitris Panagiotou <jim.panag2000@gmail.com>
2023-12-05 18:23:08 +0200

    Added four modules Elements, Collections, Views, Mods and implemented them in the app config

---
+++ src/app/elements/elements-home/elements-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elements-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './elements-home.component.html',
+  styleUrl: './elements-home.component.css'
+})
+export class ElementsHomeComponent {
+
+}
---
+++ src/app/elements/elements.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ElementsHomeComponent,
+    ElementsRoutingModule
+  ],
+  exports: [ElementsHomeComponent]
+})
+export class ElementsModule { }
