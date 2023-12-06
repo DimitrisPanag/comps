83a88fb430e7f9b56f574c95f4e3ff4a85d00ec5
Dimitris Panagiotou <jim.panag2000@gmail.com>
2023-12-06 17:26:35 +0200

    Changes fixed the routing of the modules with the app component and added the css styling semantic-ui

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, NgModule } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { elementsRoutes } from './elements/elements-routing.module';
 import { collectionsRoutes } from './collections/collections-routing.module';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
