a35730c35d53703b3ccaf4a777dd504156f5d7d6
Dimitris Panagiotou <jim.panag2000@gmail.com>
2023-12-11 17:16:47 +0200

    Changes #13 1. Added the mods module and routed it with the app.routes 2. Added the mods-home and modal component inside the mods module and specified the parent and chld components 3. Added the routing in the app.component.html

---
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ModalComponent } from '../modal/modal.component';
+
+@Component({
+  selector: 'app-mods-home',
+  standalone: true,
+  imports: [ModalComponent],
+  templateUrl: './mods-home.component.html',
+  styleUrl: './mods-home.component.css'
+})
+export class ModsHomeComponent {
+
+}
