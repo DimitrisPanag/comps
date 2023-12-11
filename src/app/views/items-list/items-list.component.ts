6ab62934894d03381d1547858b2cdb84d27f6344
Dimitris Panagiotou <jim.panag2000@gmail.com>
2023-12-11 16:57:50 +0200

    Changes #12 1. Added the items-list component and routed it with the viewHome component 2. Added some images to be placed as info in the items-list component 3. Changed the margin top in every component

---
+++ src/app/views/items-list/items-list.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-items-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './items-list.component.html',
+  styleUrl: './items-list.component.css'
+})
+export class ItemsListComponent {
+  @Input() items = [] as any[];
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+}
